8621b7909705a93fc1968c3ebb1f8464869ec1a5
Maris <marisgailitis@gmail.com>
2023-12-26 13:04:53 +0200

    7th task

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a string");
+        string userInput = Console.ReadLine();
+		int sum = CountUppercases(userInput);
+        Console.WriteLine($"The number of uppercases in the string entered is : {sum}");
+    }
+
+	private static int CountUppercases(string input)
+	{
+		int sum = 0;
+
+		foreach (char c in input)
+        {
+			if(char.IsUpper(c))
+			{
+				sum++;
+			}
+        }
+
+		return sum;
+	}
+}
