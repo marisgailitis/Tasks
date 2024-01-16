2c393634dced1c0d7c081fadde63d9a115af2be2
Maris <marisgailitis@gmail.com>
2024-01-16 16:39:25 +0200

    fix

--- csharp-basics/exercises/Loops/Exercise10/Program.cs
+++ csharp-basics/exercises/Loops/Exercise10/Program.cs
@@ -23,8 +23,11 @@ namespace Exercise10
 
 				if (numbers.Length < 2) Console.WriteLine("Two numbers are needed");
 
-				if (int.TryParse(numbers[0], out int min) && int.TryParse(numbers[1], out int max))
+				try
 				{
+					int min = int.Parse(numbers[0]);
+					int max = int.Parse(numbers[1]);
+
 					if(min > 0 && min < 10 && min < max)
 					{
 						for(int i = min; i < max + 1; i++)
@@ -34,7 +37,7 @@ namespace Exercise10
 						break;
 					}
 				}
-				else
+				catch
 				{
 					Console.WriteLine("Could not get numbers from the input");
 				}
