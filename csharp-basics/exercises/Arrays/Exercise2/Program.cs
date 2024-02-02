1b0f12bf9a50e85fa2b9a2ccfb64b2a0945cc99d
Maris <marisgailitis@gmail.com>
2024-02-02 12:58:34 +0200

    fix

--- csharp-basics/exercises/Arrays/Exercise2/Program.cs
+++ csharp-basics/exercises/Arrays/Exercise2/Program.cs
@@ -8,17 +8,20 @@ namespace Exercise2
 		{
 			Console.WriteLine("Please enter a min number");
 			int minNumber = int.Parse(Console.ReadLine());
+
 			Console.WriteLine("Please enter a max number");
-			int[] myArray = new int[int.Parse(Console.ReadLine()) - minNumber];
+			int maxNumber = int.Parse(Console.ReadLine());
+
+			int[] myArray = new int[maxNumber - minNumber];
 
 			int sum = 0;
 
-			for (int i = 0; i < myArray.Length; i++)
+			for (int i = minNumber; i < maxNumber; i++)
 			{
 				myArray[i] = i;
+
 				sum += i;
 			}
-
 			Console.WriteLine("The sum is " + sum);
 			Console.ReadKey();
 		}
